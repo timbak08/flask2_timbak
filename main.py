5b5ff3aaca6a0395ca28d0cee529acc4474c9414
NaranG <ahxgeonit@yandex.ru>
2024-02-08 17:17:46 +0300

    дорешать вторую задачу

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/<prof>')
+@app.route('/training/<prof>')
+def index1(prof):
+    return render_template('training.html', prof=prof)
+
+
 
 if __name__ == '__main__':
     app.run(port=8001, host='127.0.0.1')
